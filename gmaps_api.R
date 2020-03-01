8b1ac2b0a96ba5362527b5bcdb051dae88ccec66
chriztopherton <christopherton1998@gmail.com>
2020-03-01 02:59:33 -0800

    input reactivity

--- gmaps_api.R
+++ gmaps_api.R
@@ -39,7 +39,7 @@ yelp_key = Sys.getenv("yelp_key")
 #---------------------------------------------------------------------------------------------------------
 
 
-ui <- fluidPage(theme=shinytheme("spacelab"),
+ui <- fluidPage(theme=shinytheme("darkly"),
                 
                 titlePanel("Google Maps API"),
     
@@ -59,9 +59,14 @@ ui <- fluidPage(theme=shinytheme("spacelab"),
                     
                     searchInput
                     (
-                      inputId = "origin",label = strong("Start:"),btnSearch = icon("search"),
+                      inputId = "origin",label = strong("Start:"),value = "Davis",btnSearch = icon("search"),
                       btnReset = icon("remove"),width = "450px"
                     ),
+                    
+                    # textInput(
+                    #   inputId = "origin",
+                    #   label = strong("Start:")
+                    # ),
                     
                     # user input for Point B
                     # selectInput
@@ -76,9 +81,14 @@ ui <- fluidPage(theme=shinytheme("spacelab"),
                     
                     searchInput
                     (
-                      inputId = "destination",label = strong("End:"),btnSearch = icon("search"),
+                      inputId = "destination",label = strong("End:"),value = "Sacramento",btnSearch = icon("search"),
                       btnReset = icon("remove"),width = "450px"
                     ),
+                    
+                    # textInput(
+                    #   inputId = "destination",
+                    #   label = strong("End:")
+                    # ),
                     
                     #whether or not distance matrix is shown
                     checkboxInput
@@ -86,7 +96,7 @@ ui <- fluidPage(theme=shinytheme("spacelab"),
                       inputId = "show_distance",
                       label = strong("Show Distance"),
                       value = TRUE)
-                  ),
+                    ),
                   
                   #----------------------------------------------------------------------------------------------------
                   mainPanel
@@ -105,7 +115,7 @@ ui <- fluidPage(theme=shinytheme("spacelab"),
                   )
                 
 #------------------------------------------------------------------------------------------------------------------------
-server <- function(input, output) {
+server <- function(input, output, session) {
   
   doc = reactive({mp_directions(origin = input$origin, #from UCD Pav
                       destination = input$destination,
@@ -121,14 +131,16 @@ server <- function(input, output) {
   #maps + route
   
   #given response object, use mp_get_routes to create a spatial layer of route lines
-  r = reactive({mp_get_routes(doc())})
+  #r = reactive({mp_get_routes(doc())})
   #print(r)
   
   seg = reactive({mp_get_segments(doc())})
   #print(seg)
   
   #plots coordinates and directions
-  pal = reactive({colorFactor(palette = sample(colors(), length(unique(seg()$segment_id))),domain = seg()$segment_id)})
+  #pal = reactive({colorFactor(palette = sample(colors(), length(unique(seg()$segment_id))),domain = seg()$segment_id)})
+  
+  
   
   output$map <- renderLeaflet({
     req(doc())
@@ -160,22 +172,22 @@ server <- function(input, output) {
   output$distancetable <- renderTable({
      
     locations = c(input$origin,input$destination)
-    # dist = mp_matrix(origins= locations, 
-    #                   destinations = locations,
-    #                   key = paste(maps_key))
-    #  
-    # m1 = mp_get_matrix(dist, value = "distance_text") 
-    #  colnames(m1) = locations 
-    #  rownames(m1) = locations 
-    # 
-    #  m2 = mp_get_matrix(dist, value = "duration_text") 
-    #  colnames(m1) = locations 
-    #  rownames(m1) = locations 
-    #  
-    #  if(input$show_distance){
-    #    DT::datatable(data = m1,rownames = FALSE) 
-    #    m2
-    #    }
+    dist = mp_matrix(origins= locations,
+                      destinations = locations,
+                      key = paste(maps_key))
+
+    m1 = mp_get_matrix(dist, value = "distance_text")
+     colnames(m1) = locations
+     rownames(m1) = locations
+
+     m2 = mp_get_matrix(dist, value = "duration_text")
+     colnames(m1) = locations
+     rownames(m1) = locations
+
+     if(input$show_distance){
+       DT::datatable(data = m1,rownames = FALSE)
+       m2
+       }
 })
   
   #------------------------------------------------------------------------------------------------------------------------
